aa7f1dad8a70d98e59fe076621e49fbdb7c82902
ali <aliamr582005@gmail.com>
2026-03-11 23:06:40 +0200

    added tree parsing logic and fixed print

--- Java/phase2.java
+++ Java/phase2.java
@@ -1,5 +1,8 @@
 package Java;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 abstract class ASTNode {
@@ -20,8 +23,8 @@ class NumberNode extends ASTNode {
     }
 
     @Override
-    public void print(String prefix){
-        System.out.println(prefix + value);
+    public void print(String prefix) {
+        System.out.println(prefix + "|-- " + value);
     }
 }
 
@@ -43,9 +46,9 @@ class AddNode extends ASTNode {
 
     @Override
     public void print(String prefix){
-        System.out.println(prefix + '+');
-        left.print(prefix + " |--");
-        right.print(prefix + " \\--");
+        System.out.println(prefix + "|-- +");
+        left.print(prefix + "    ");
+        right.print(prefix + "    ");
     }
 }
 class SubtractNode extends ASTNode {
@@ -65,9 +68,9 @@ class SubtractNode extends ASTNode {
 
     @Override
     public void print(String prefix){
-        System.out.println(prefix + '-');
-        left.print(prefix + " |--");
-        right.print(prefix + " \\--");
+        System.out.println(prefix + "|-- -");
+        left.print(prefix + "    ");
+        right.print(prefix + "    ");
     }
 }
 class MultiplyNode extends ASTNode {
@@ -87,9 +90,9 @@ class MultiplyNode extends ASTNode {
 
     @Override
     public void print(String prefix){
-        System.out.println(prefix + '*');
-        left.print(prefix + " |--");
-        right.print(prefix + " \\--");
+        System.out.println(prefix + "|-- *");
+        left.print(prefix + "    ");
+        right.print(prefix + "    ");
     }
 }
 class DivideNode extends ASTNode {
@@ -106,28 +109,67 @@ class DivideNode extends ASTNode {
     public int evaluate(){
         int r = right.evaluate();
         if(r==0) throw new IllegalArgumentException("division by zero");
-        return left.evaluate() / right.evaluate();
+        return left.evaluate() / r;
     }
 
     @Override
     public void print(String prefix){
-        System.out.println(prefix + '/');
-        left.print(prefix + " |--");
-        right.print(prefix + " \\--");
+        System.out.println(prefix + "|-- /");
+        left.print(prefix + "    ");
+        right.print(prefix + "    ");
     }
 }
 
 public class phase2 {
+    
+    public static ASTNode parseFactor(Queue<String> tokens) {
+        String token = tokens.poll();
+        if (token.equals("(")) {
+            ASTNode node = parseArith(tokens);
+            tokens.poll();
+            return node;
+        }
+        return new NumberNode(Integer.parseInt(token));
+    }
+
+    public static ASTNode parseExpression(Queue<String> tokens) {
+        ASTNode left = parseFactor(tokens);
+        while (!tokens.isEmpty() && (tokens.peek().equals("*") || tokens.peek().equals("/"))) {
+            String op = tokens.poll();
+            ASTNode right = parseFactor(tokens);
+            if (op.equals("*")) {
+                left = new MultiplyNode(left, right);
+            } else {
+                left = new DivideNode(left, right);
+            }
+        }
+        return left;
+    }
+
+    public static ASTNode parseArith(Queue<String> tokens) {
+        ASTNode left = parseExpression(tokens);
+        while (!tokens.isEmpty() && (tokens.peek().equals("+") || tokens.peek().equals("-"))) {
+            String op = tokens.poll();
+            ASTNode right = parseExpression(tokens);
+            if (op.equals("+")) {
+                left = new AddNode(left, right);
+            } else {
+                left = new SubtractNode(left, right);
+            }
+        }
+        return left;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter an expression: ");
+        System.out.println("Enter an expression (space-separated tokens): ");
         String input = scanner.nextLine();
         scanner.close();
-        String[] tokens = input.split(" ");
-        // parse to AST
-
-        // tree.print("")
-
-        // tree.evaluate()
+        Queue<String> tokens = new LinkedList<>(Arrays.asList(input.split(" ")));
+        
+        ASTNode tree = parseArith(tokens);
+        System.out.println("\nAST: ");
+        tree.print("");
+        System.out.println("\nResult = " + tree.evaluate());
     }
 }
